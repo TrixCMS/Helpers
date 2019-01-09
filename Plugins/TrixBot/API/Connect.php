82845de9f05d90507995188009cfac338d002230
TrixCMS <46542138+TrixCMS@users.noreply.github.com>
2019-01-09 22:39:28 +0100

    Create Connect.php

---
+++ Plugins/TrixBot/API/Connect.php
@@ -0,0 +1,37 @@
+<?php
+class APIConnect extends Controller
+{
+    // Documentation : https://docs.trixcms.eu/
+    protected $guzzle;
+    /**
+     * APIConnect constructor.
+     */
+    public function __construct()
+    {
+        return $this->guzzle = (new Client(['base_uri' => 'urlOfAPI', 'timeout' => 5, 'verify' => base_path() . '/cacert.pem']));
+    }
+    /**
+     * @return string
+     * @throws APIConnectFail
+     */
+    public function Login()
+    {
+        try {
+            $request = $this->guzzle->request('GET', "?key=" . env('APP_KCFT'));
+            $content = $request->getBody();
+            return $content->getContents();
+        } catch (GuzzleException $e) {
+            Log::info('APIConnect has failed.');
+            throw new APIConnectFail('APIConnect has failed.');
+        }
+    }
+    /**
+     * @param $item
+     * @return mixed
+     * @throws APIConnectFail
+     */
+    public function getItem($item)
+    {
+        if (!empty(json_decode($this->Login(), true)[$item])) return json_decode($this->Login(), true)[$item];
+    }
+}
